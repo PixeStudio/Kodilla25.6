9f82c21e3334ff74896290a35d3ca012f3f87119
Darek <56841910+PixeStudio@users.noreply.github.com>
2019-11-22 00:29:48 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.get('/', function(req, res){
+    res.render('login');
+});
+
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.get('/logged', function(req, res){
+    res.render('logged', {
+        user: req.query.login
+    });
+});
+
+app.listen(3000, function() {
+    console.log('Aplikacja nasłuchuje porcie 3000');
+});
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
